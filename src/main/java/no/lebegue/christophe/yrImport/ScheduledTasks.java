855cbfd363497269f8be76e5d178be594d140b55
tof <christophe.lebegue@gmail.com>
2018-04-26 20:02:13 +0200

    use unicode character in URL

--- src/main/java/no/lebegue/christophe/yrImport/ScheduledTasks.java
+++ src/main/java/no/lebegue/christophe/yrImport/ScheduledTasks.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,67 +26,93 @@ import org.w3c.dom.NodeList;
 @Component
 public class ScheduledTasks {
 
-    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-    private static final SimpleDateFormat dateFormatXML = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-    private static final SimpleDateFormat dateFormatDate = new SimpleDateFormat("yyyy-MM-dd");
-    private static final SimpleDateFormat dateFormatDatetime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    
-    String fromFile="https://www.yr.no/sted/Norge/Hedmark/Løten/Jønsrud/varsel.xml";
-    String toFile="/volume1/yrVarsel/JonsrudVarsel-";
-    
-    @Scheduled(fixedRate = 3600000)
-    public void importAndArchiveFile() 
-    {	
-    	 toFile = toFile + dateFormat.format(new Date())+".xml";
-    	 
-         try {
-        	 FileUtils.copyURLToFile(new URL(fromFile), new File(toFile));
-        	 log.info("File archived into " + toFile);
-        	 parseFile(toFile);
-         } catch (IOException e ) {
-             e.printStackTrace();
-         }	
-         
-    }
-    
-	public void parseFile(String file) {
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+	private static final SimpleDateFormat dateFormatXML = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+	private static final SimpleDateFormat dateFormatDate = new SimpleDateFormat("yyyy-MM-dd");
+	private static final SimpleDateFormat dateFormatDatetime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	String fromFile="https://www.yr.no/sted/Norge/Hedmark/L\u00F8ten/J\u00F8nsrud/varsel.xml";
+	String toFolder="/volume1/yrVarsel/";
+	String toFile="JonsrudVarsel-";
+	
+	@Scheduled(fixedRate = 3600000)
+	public void importAndArchiveFile() 
+	{	
+		if(YrImportApplication.isImportFile()) {
+			String outputFile = toFolder+toFile + dateFormat.format(new Date())+".xml";
+			try {
+				FileUtils.copyURLToFile(new URL(fromFile), new File(outputFile));
+				log.info("File archived into " + outputFile);
+			} catch (IOException e ) {
+				e.printStackTrace();
+			}	
+		}else {
+			log.info("Skip import file");
+		}
+
+	}
+	
+	
+	
+	@Scheduled(fixedRate = 3600000, initialDelay = 10000)
+	public void searchFileToParse() {
+		if(YrImportApplication.isParseFile()) {
+			File folder = new File(toFolder); 
+			File file;
+			if(folder.isDirectory()){
+		        List<File> files = Arrays.asList(folder.listFiles());
+		        Iterator<File> fileIterator = files.iterator();
+		        while(fileIterator.hasNext()){
+		        	file = fileIterator.next();
+		            if(file.getName().startsWith(toFile)) {
+		            	parseFile(file);
+		            	file.renameTo(new File(toFolder+"processed/"+file.getName()));
+		            }
+		        }
+		    }
+		}else {
+			log.info("Skip parse file");
+		}
+	}
+	
+	public void parseFile(File XMLFile) {
 
 		try {
-
-			File fXmlFile = new File(file);
+			
+		
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
+			Document doc = dBuilder.parse(XMLFile);
 
 			//optional, but recommended
 			//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 			doc.getDocumentElement().normalize();
 
-			
+
 			NodeList nList = doc.getElementsByTagName("sun");
 			Node node = nList.item(0);
-			
+
 			Date sunrise = dateFormatXML.parse(node.getAttributes().item(0).getNodeValue());
 			Date sunset = dateFormatXML.parse(node.getAttributes().item(1).getNodeValue());
-			
+
 			String sql = "INSERT INTO sun VALUES ('"+dateFormatDate.format(sunrise)+"','"+dateFormatDatetime.format(sunrise)+"','"+dateFormatDatetime.format(sunset)+"') ON DUPLICATE KEY UPDATE sunrise='"+dateFormatDatetime.format(sunrise)+"', sunset = '"+dateFormatDatetime.format(sunset)+"'";
 			log.info(sql);
 			jdbcTemplate.execute(sql);
-			
+
 			nList = doc.getElementsByTagName("lastupdate");
 			node = nList.item(0);
 			System.out.println(node.getTextContent());
-			
-			
-			
-			
+
+
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
+
 }
--- src/main/java/no/lebegue/christophe/yrImport/YrImportApplication.java
+++ src/main/java/no/lebegue/christophe/yrImport/YrImportApplication.java
@@ -9,8 +9,25 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAutoConfiguration
 public class YrImportApplication {
+	private static boolean importFile = false;
+	private static boolean parseFile = false;
+	
+	public static boolean isImportFile() {
+		return importFile;
+	}
 
+	public static boolean isParseFile() {
+		return parseFile;
+	}
 	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-i")) {
+				importFile = true;
+			}else if(args[i].equals("-p")){
+				parseFile = true;
+			}
+		}
+		
 		SpringApplication.run(YrImportApplication.class, args);
 	}
 }
